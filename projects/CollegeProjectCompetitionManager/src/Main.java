78f06d79d1bc55ac61abfdb86ba7b1cc0bb3667d
email@female.com <surajshah1509>
2026-03-16 21:42:11 +0530

    commit suraj

--- projects/CollegeProjectCompetitionManager/src/Main.java
+++ projects/CollegeProjectCompetitionManager/src/Main.java
@@ -3,61 +3,54 @@ package projects.CollegeProjectCompetitionManager.src;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-
         CompetitionManager manager = new CompetitionManager();
-        
-        System.out.println("Welcome to College Project Competition Manager");
-
-
-        System.out.println("Enter the number of commands:");
 
         int N = sc.nextInt();
+        sc.nextLine(); // buffer clear
 
-        for(int i=0;i<N;i++){
-            String commands = sc.nextLine();
+        for (int i = 0; i < N; i++) {
 
-            sc.nextLine();
+            String line = sc.nextLine(); // full command line
+            String parts[] = line.split(" ");
 
-            if(commands.equals("REGISTER")){
-                System.out.println("Enter the team id:");
-                String teamId = sc.next();
+            String command = parts[0];
 
-                System.out.println("Enter the section:");
-                String section = sc.next();
+            if (command.equals("REGISTER")) {
 
-                System.out.println("Enter the domain:");
-                String domain = sc.next();
-
-                System.out.println("Enter the project name:");
-                String projectName = sc.next();
-
-                System.out.println("Enter the project score:");
-                int projectScore = sc.nextInt();
+                String teamId = parts[1];
+                String section = parts[2];
+                String domain = parts[3];
+                String projectName = parts[4];
+                int projectScore = Integer.parseInt(parts[5]);
 
                 manager.registerTeam(teamId, section, domain, projectName, projectScore);
-
             }
-            else if(commands.equals("REVISE")){
-                System.out.println("Enter the team id:");
-                String teamId = sc.next();
 
-                System.out.println("Enter the project score:");
-                int projectScore = sc.nextInt();
+            else if (command.equals("REVISE")) {
+
+                String teamId = parts[1];
+                int projectScore = Integer.parseInt(parts[2]);
 
                 manager.reviseSocre(teamId, projectScore);
             }
-            else if(commands.equals("FILTERDOMAIN")){
-                System.out.println("Enter the domain:");
-                String domain = sc.next();
+
+            else if (command.equals("FILTERDOMAIN")) {
+
+                String domain = parts[1];
                 manager.filterByDomain(domain);
             }
-            else if(commands.equals("QUALIFY")){
-                System.out.println("Enter the cutoff value:");
-                int cutoff = sc.nextInt();
+
+            else if (command.equals("QUALIFY")) {
+
+                int cutoff = Integer.parseInt(parts[1]);
                 manager.qualifyTeams(cutoff);
             }
         }
+
+        sc.close();
     }
 }
